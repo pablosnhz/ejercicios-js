2cc8408b51b50492cfcfd28b9036009b8c8cecf7
PabloSnhz <pablosanhuezaaa@gmail.com>
2023-09-21 21:11:25 -0300

    primeraParte

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1 Escribe Hola mundo! por pantalla!.
+// 2 Crea una funcion que se cargue al inicio
+// y que muestre la suma de dos numeros.
+// 3 Muestra un mensaje por consola.
+
+
+/* 1
+ document.write('Hola mundo!'); */
+/* 2
+function sumaNumeros(){
+    let num1 = 5;
+    let num2 = 10;
+
+    document.write(num1 + num2);
+}   onload=sumaNumeros en el donde muestra el resultado */ 
+
+function sumaNumeros(){
+    let num1 = 5;
+    let num2 = 10;
+
+    document.write(num1 + num2);
+    console.log('sumaNumeros');
+}
